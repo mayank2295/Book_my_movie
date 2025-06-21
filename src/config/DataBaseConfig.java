1175bcf9619eee92c192882f8dc8296c0250951b
mayank <mayankgupta23081@gmail.com>
2025-06-22 03:12:21 +0530

    Initial commit

---
+++ src/config/DataBaseConfig.java
@@ -0,0 +1,18 @@
+
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class DataBaseConfig {
+        static String url="jdbc:mysql://localhost:3306/bookmymovies";
+        static String username="root";
+        static String password="@321Mayank";
+
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(url,username,password);
+        }
+    }
+
+
